b7a6f90dc7278054ae34278cd87d0cc7a66d7c13
Rafael Carneiro <rafaferreiracarneiro@gmail.com>
2020-05-06 12:03:50 -0300

    new  price-service: create test structure

---
+++ services/price/test/Price.Domain.Tests/DomainTestBase.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using EventFlow;
+using EventFlow.Aggregates;
+using EventFlow.Configuration;
+using EventFlow.Core;
+using EventFlow.DependencyInjection.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Price.Domain.Tests
+{
+	public class DomainTestBase : IDisposable
+{
+		protected readonly IRootResolver Resolver;
+		protected readonly IAggregateStore AggregateStore;
+
+		public DomainTestBase()
+		{
+			var services = new ServiceCollection();
+			ConfigurationRootCreator.Create(services);
+			
+			Resolver = EventFlowOptions.New
+				.UseServiceCollection(services)
+				.RegisterModule<PriceDomainModule>()                
+				.CreateResolver();
+
+			AggregateStore = Resolver.Resolve<IAggregateStore>();
+		}
+
+		protected async Task UpdateAsync<TAggregate, TIdentity>(
+			TIdentity id, 
+			Action<TAggregate> action)
+				where TAggregate : IAggregateRoot<TIdentity>
+				where TIdentity : IIdentity
+		{
+			await AggregateStore
+				.UpdateAsync<TAggregate, TIdentity>(
+					id,
+					SourceId.New,
+					(a, c) => { 
+						action(a);
+						return Task.FromResult(0);
+					},
+					CancellationToken.None)
+				.ConfigureAwait(false);
+		}
+
+		public void Dispose()
+		{
+			Resolver?.Dispose();
+		}
+	}
+}
