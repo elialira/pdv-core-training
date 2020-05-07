6767a7460ddcc44f4e1cf1e766ddfcefad5e0ea1
Rafael Carneiro <rafaferreiracarneiro@gmail.com>
2020-05-07 11:44:19 -0300

    chg price-service: add new application contracts

--- services/price/src/Price.Application/Interfaces/IPriceTableService.cs
+++ services/price/src/Price.Application/Interfaces/IPriceTableService.cs
@@ -1,11 +1,17 @@
+using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Price.Domain.PriceTable;
 
 namespace Price.Application.Interfaces
 {
-    public interface IPriceTableService
-    {
-         Task<PriceTableId> Create(CancellationToken cancellationToken);
-    }
+	public interface IPriceTableService
+	{
+		Task<PriceTableId> Create(CancellationToken cancellationToken);
+		Task<IEnumerable<PriceTable>> GetAll(CancellationToken cancellationToken);
+		Task<PriceTable> GetById(Guid id, CancellationToken cancellationToken);
+		Task<PriceTableId> Update(CancellationToken cancellationToken);
+		Task<PriceTableId> Remove(Guid id);
+	}
 }
--- services/price/src/Price.Application/Services/PriceTableService.cs
+++ services/price/src/Price.Application/Services/PriceTableService.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using EventFlow;
+using EventFlow.Queries;
 using Price.Application.Interfaces;
 using Price.Domain.PriceTable;
 using Price.Domain.PriceTable.Commands;
@@ -12,13 +14,19 @@ namespace Price.Application.Services
   public class PriceTableService : IPriceTableService
   {
     private readonly ICommandBus _commandBus;
+    private readonly IQueryProcessor _queryProcessor;
     
-    public PriceTableService(ICommandBus commandBus)
-      => _commandBus = commandBus;
+    public PriceTableService(
+      ICommandBus commandBus, 
+      IQueryProcessor queryProcessor)
+    {
+      _commandBus = commandBus;
+      _queryProcessor = queryProcessor;
+    }
     
     public async Task<PriceTableId> Create(CancellationToken cancellationToken)
     {
-      var id = PriceTableId.New;
+      var id = PriceTableId.New;      
       
       await _commandBus
         .PublishAsync(
@@ -27,8 +35,28 @@ namespace Price.Application.Services
           ), 
           cancellationToken)
         .ConfigureAwait(false);
-
+      
       return id;
+    }
+
+    public Task<IEnumerable<PriceTable>> GetAll(CancellationToken cancellationToken)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<PriceTable> GetById(Guid id, CancellationToken cancellationToken)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<PriceTableId> Remove(Guid id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<PriceTableId> Update(CancellationToken cancellationToken)
+    {
+      throw new NotImplementedException();
     }
   }
 }
