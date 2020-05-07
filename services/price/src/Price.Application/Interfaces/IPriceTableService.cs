0922a70944cbf16c54059044b417c7a57de702fe
Anderson Martins <andersonom@gmail.com>
2020-05-07 18:19:48 -0300

    fix interface identation

--- services/price/src/Price.Application/Interfaces/IPriceTableService.cs
+++ services/price/src/Price.Application/Interfaces/IPriceTableService.cs
@@ -10,8 +10,9 @@ namespace Price.Application.Interfaces
 {
 	public interface IPriceTableService
 	{
-		Task<PriceTableId> Create(string name, List<ProductPrice> productPrice, 
-											CancellationToken cancellationToken);
+		Task<PriceTableId> Create(string name, 
+								  List<ProductPrice> productPrice, 
+							      CancellationToken cancellationToken);
 		Task<IEnumerable<PriceTableReadModel>> GetAll(CancellationToken cancellationToken);
 		Task<PriceTableReadModel> GetById(string id, CancellationToken cancellationToken);
 		Task<PriceTableId> Update(CancellationToken cancellationToken);
